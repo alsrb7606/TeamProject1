66f18c15be63253143cf16a3492fef12edc2e79e
mglee <alsrb7606@naver.com>
2021-06-29 21:22:44 +0900

    first commit

---
+++ Teamproject1/src/application/AppMain.java
@@ -0,0 +1,32 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+
+import javafx.stage.Stage;
+
+public class AppMain extends Application{
+
+	public static void main(String[] args) {
+		launch(args);
+		
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+		
+		Scene scene = new Scene(root);
+		
+		primaryStage.setTitle("주차관리시스템");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
+	}
+
+}
---
+++ Teamproject1/src/module-info.java
@@ -0,0 +1,7 @@
+module Teamproject1 {
+	requires javafx.controls;
+	requires javafx.graphics;
+	requires javafx.fxml;
+	
+	opens application to javafx.graphics, javafx.fxml;
+}
