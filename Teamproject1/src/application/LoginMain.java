9fcdfdfb01650a49cd2b8e5b98e0219591200e52
HP <HP@DIP-PC>
2021-07-07 09:57:00 +0900

    주석 추가

--- Teamproject1/src/application/LoginMain.java
+++ Teamproject1/src/application/LoginMain.java
@@ -10,15 +10,14 @@ public class LoginMain extends Application{
 
 	public static void main(String[] args) {
 		
-		
 		//Cardata 객체 생성.
 		Data[] dataArray = {new Data("1"), new Data("2"), new Data("3"), new Data("4"), new Data("5"), new Data("6"), new Data("7"), new Data("8"), 
 				new Data("9"), new Data("10"), new Data("11"), new Data("12"), };
-		for(int i=1; i<=12; i++) {
+		//해쉬맵에다 각 자리에 Data객체 저장.
+		for(int i=1; i<=12; i++) {	
 		CarData.hashMap.put(Integer.toString(i), dataArray[i-1]);
 		}
 
-		
 		launch(args);
 		
 	}
